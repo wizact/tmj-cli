1c42e30891f4ea3007413be1dc4ad402d39b253d
Amir <amir.mohtasebi@gmail.com>
2016-07-10 23:43:39 +1200

    add category endpoing test call

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
 "use strict";
+var request = require("request");
+request.get("https://api.tmsandbox.co.nz/v1/Categories/5000.json", function (error, response, body) {
+    console.log(response);
+    var bodyResponse = JSON.parse(body);
+    console.log(bodyResponse.Name);
+});
 console.log("test");
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,10 @@
 import * as request from "request";
 
+request.get("https://api.tmsandbox.co.nz/v1/Categories/5000.json", 
+function (error, response, body) {
+   console.log(response);
+   let bodyResponse = JSON.parse(body);
+   console.log(bodyResponse.Name); 
+});
+
 console.log("test");
